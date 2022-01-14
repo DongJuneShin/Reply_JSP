c84d573063af5b54ee1c28c1d9aee585ff53c5c5
DongJuneShin <DongJuneShin@gmail.com>
2022-01-14 23:45:08 +0900

    write.jsp에서 doWrite에 잘 넘어오는지 확인

--- src/main/java/com/sdj/exam/exam2/servlet/UsrArticleDoWriteServlet.java
+++ src/main/java/com/sdj/exam/exam2/servlet/UsrArticleDoWriteServlet.java
@@ -1,14 +1,16 @@
 package com.sdj.exam.exam2.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/usr/home/main")
-public class UsrHomeMainServlet extends HttpServlet {
+@WebServlet("/usr/article/doWrite")
+public class UsrArticleDoWriteServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 들어오는 파리미터를 UTF-8로 해석
@@ -19,7 +21,14 @@ public class UsrHomeMainServlet extends HttpServlet {
 		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
 		response.setContentType("text/html; charset=UTF-8");
 
+		//write.jsp에서 잘 넘어오는지 확인 하는 코드
+		String title=request.getParameter("title");
+		String body=request.getParameter("body");
 		
+		
+		response.getWriter().append(title + "<br>");
+		
+		response.getWriter().append(body + "<br>");
 	}
 
 
